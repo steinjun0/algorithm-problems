8ab8ee34da16dbdb6c9e76b1e1060cd77d8fd314
steinjun0 <54354459+steinjun0@users.noreply.github.com>
2020-08-10 19:24:55 +0900

    08-10828 Study finish

--- 08-10828(Stack).py
+++ 08-10828(Stack).py
@@ -3,6 +3,30 @@
 # 18:29 ~ 18:42
 # 12+13 = takes 25 mins
 
+
+# 순위권 코드
+'''
+from sys import stdin
+
+stack = []
+next(stdin)                     # next(iterable) => iterable한 객체에서 인자 하나를 빼내는 메소드. 연속으로 쓰면 index를 늘리면서 출력된다
+                                # 여기서는 N번 반복되는 횟수를 사용하지 않기에, 값을 버리기 위해 사용
+
+for line in stdin:              # for line in stdin: => 이렇게 작성하면 한 줄씩 들어오는 입력을 받아서 line에 넣을 수 있다.
+                                # stdin.readline() 을 하면 딱 한 번만 받지만 stdin은 게속해서 받고 ^Z를 입력받아야 멈춘다.
+                                # 본 문제에서는 반복을 멈출 필요가 없어서 중단점을 없애고 계속 입력을 받는다
+    command = line.split()      # 이 뒤는 나와 코드가 같다
+    if command[0] == 'push':
+        stack.append(command[1])
+
+    ~~~~~~
+
+    elif command[0] == 'top':
+        if stack: print(stack[-1])
+        else: print(-1)
+'''
+# (위 코드는 입력과 출력이 혼재해서 출력되는데 그래도 통과는 되나보다)
+
 import sys
 from sys import stdin, stdout
 
--- toSubmit.py
+++ toSubmit.py
@@ -1,89 +1,19 @@
-'''
-200803 18:42 ~ 19:02, 20:06 ~ 20:38,(20:38 문제 잘못 읽은걸 깨달음)
-N은 행의 개수
-M은 열의 개수
-N, M < 50
-'''
+from sys import stdin
 
-import sys
-from sys import stdin, stdout
-
-N, M = list(map(int, sys.stdin.readline().rstrip().split()))
-ramps = []
-
-for i in range(N):
-    ramps.append(list(map(int, sys.stdin.readline().rstrip())))
-
-K = int(sys.stdin.readline().rstrip())
-
-row_sums = [M for i in range(N)]
-
-for i in range(N):
-    for j in range(M):
-        row_sums[i] -= int(ramps[i][j])
-    
-row_sums.sort()
-
-
-
-categroized_rows = []
-temp_zeros = []
-temp_searched_col = []
-
-
-def make_group(uncategorized_list, searched_col):
-    global ramps
-    global categroized_rows
-    global temp_zeros
-
-    zeros = []
-    ones = []
-
-    
-    for row_number in uncategorized_list:
-        if int(ramps[row_number][searched_col]) == 1:
-            ones.append(row_number)
-        elif int(ramps[row_number][searched_col]) == 0:
-            zeros.append(row_number)
-    
-    if searched_col+1 == M:
-        if len(ones) > 1:
-            categroized_rows.append(ones)
-
-        if len(zeros) > 1:
-            categroized_rows.append(zeros)
-
-    elif searched_col+1 < M:
-        if len(ones) > 1:
-            if len(ones) > 1:
-                temp_zeros.append(zeros)
-                temp_searched_col.append(searched_col)
-                make_group(ones, searched_col + 1)
-                
-            if len(temp_zeros) > 0:
-                zeros = temp_zeros.pop()
-                searched_col = temp_searched_col.pop()
-
-            if len(zeros) > 1:
-                make_group(zeros, searched_col + 1)
-        elif len(zeros) > 1:
-            if len(zeros) > 1:
-                make_group(zeros, searched_col + 1)
-    
-
-make_group([i for i in range(N)], 0)
-
-
-length_categorized_rows = []
-
-for i in categroized_rows:
-    length_categorized_rows.append(len(i))
-
-length_categorized_rows.sort()
-
-
-if len(length_categorized_rows) > 0:
-    if length_categorized_rows[0] > row_sums[0]:
-        print(length_categorized_rows[0])
-    else:
-        print(row_sums[0])
+stack = []
+next(stdin)
+for line in stdin:
+    command = line.split()
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        if stack: print(stack.pop())
+        else: print(-1)
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if stack: print(0)
+        else: print(1)
+    elif command[0] == 'top':
+        if stack: print(stack[-1])
+        else: print(-1)
